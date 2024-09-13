e0ec47f77442107c511d5d6c2a21038b319b6b09
Axit <axithhummar4@gmail.com>
2024-09-13 11:39:01 +0530

    update DashBoard.jsx, SideBar.jsx and src/index.css

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -1,36 +1,10 @@
 import React from "react";
 import SideBar from "./SideBar";
-import { Routes } from "react-router-dom";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import { Menu } from "lucide-react";
 
 const DashBoard = () => {
   return (
     <div>
-      {/* <SideBar /> */}
-      <nav className="bg-foreground p-2">
-        <Sheet className="dark">
-          <SheetTrigger className="border-2 border-primary rounded-md p-0.5 hover:opacity-80 transition">
-            <Menu className="text-primary" />
-          </SheetTrigger>
-          <SheetContent side={"left"} className="w-[300px]">
-            <SheetHeader>
-              <div className="text-3xl font-extrabold">
-                <span className="text-primary">Biz</span>
-                <span>Grow</span>
-              </div>
-            </SheetHeader>
-          </SheetContent>
-        </Sheet>
-      </nav>
-      <Routes></Routes>
+      <SideBar />
     </div>
   );
 };
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,78 @@
-import React from "react";
+import {
+  ChartLine,
+  ChevronLeft,
+  LayoutDashboard,
+  Menu,
+  ReceiptIndianRupee,
+  ShoppingBag,
+  UsersRound,
+} from "lucide-react";
+import React, { useState } from "react";
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      className={`h-screen w-full p-3 overflow-hidden ${
+        isOpen ? "max-w-[300px]" : "max-w-[60px]"
+      } bg-black transition-all duration-300 flex flex-col items-center`}
+    >
+      <div className={`${isOpen ? "ml-auto" : "mx-auto"} mr-2`}>
+        {!isOpen ? (
+          <Menu
+            onClick={() => setIsOpen(!isOpen)}
+            className={`text-primary h-10 w-10 transition duration-150 hover:bg-white/20 rounded`}
+          />
+        ) : (
+          <ChevronLeft
+            onClick={() => setIsOpen(!isOpen)}
+            className={`text-primary h-10 w-10 transition duration-150 hover:bg-white/20 rounded`}
+          />
+        )}
+      </div>
+
+      {isOpen && (
+        <div className="text-3xl font-extrabold text-background absolute top-3 left-3">
+          <span className="text-primary">Biz</span>Grow
+        </div>
+      )}
+      <div className="w-full text-white mt-5 flex flex-col gap-5">
+        <div
+          className={`w-full hover:bg-white/20 flex gap-3.5 rounded items-center p-1 py-2 transition duration-150 ${
+            isOpen ? "px-4" : "px-0.5"
+          } text-lg`}
+        >
+          <LayoutDashboard className="text-background h-8 w-8 shrink-0" />
+          Dashboard
+        </div>
+        <div
+          className={`w-full hover:bg-white/20 flex gap-3.5 rounded items-center p-1 py-2 transition duration-150 ${
+            isOpen ? "px-4" : "px-0.5"
+          } text-lg`}
+        >
+          <UsersRound className="text-background h-8 w-8 shrink-0" />
+          Customer
+        </div>
+        <div
+          className={`w-full hover:bg-white/20 flex gap-3.5 rounded items-center p-1 py-2 transition duration-150 ${
+            isOpen ? "px-4" : "px-0.5"
+          } text-lg`}
+        >
+          <ShoppingBag className="text-background h-8 w-8 shrink-0" />
+          Items
+        </div>
+        <div
+          className={`w-full hover:bg-white/20 flex gap-3.5 rounded items-center p-1 py-2 transition duration-150 ${
+            isOpen ? "px-4" : "px-0.5"
+          } text-lg`}
+        >
+          <ReceiptIndianRupee className="text-background h-8 w-8 shrink-0" />
+          Invoices
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
